132b3800d188cfdefcd0b299aa1a2116886a1df7
Karthikakkireddy <karthikakkireddy5@gmail.com>
2026-02-04 16:40:22 +0530

    double vs float datatype

---
+++ double_vs_float/double_vs_float.java
@@ -0,0 +1,13 @@
+public class double_vs_float {
+    public static void main(String[] args) {
+       float f = 0.123456789f;
+        double d = 0.123456789;
+
+            System.out.println(f);
+            System.out.println(d);
+
+
+        
+    }
+    
+}
