4592217bb3ccf8ca3f264c18570888318d0a71d2
Karthikakkireddy <karthikakkireddy5@gmail.com>
2026-02-05 21:55:04 +0530

    PROBLEM : Implementing two interfaces with default method for same name

--- Default_Classes/Demo.java
+++ Default_Classes/Demo.java
@@ -1,28 +1,42 @@
 package Default_Classes;
-
+//ERROR Demo.java:21: error: types A and D are incompatible;
+// class B implements A, D{
+// ^
+//   class B inherits unrelated defaults for dance() from types A and D
+// 1 error
+ 
 
 
 interface A{
     void show();
+    default void dance()
+    {
+        System.out.println("Dance from interface A");
+    }
 
 }
-class B implements A{
+
+interface D
+{
+    default void dance()
+    {
+        System.out.println("Dance from interface D");
+    }
+}
+class B implements A, D{
     public void show()
     {
         System.out.println("HI b");
     }
-
-    public void sing()
-    {
-         System.out.println("SINGING ----- ");
-    }
+ 
 }
 public class Demo {
 
     public static void main(String[] args) {
-        A obj = new B();
+        B obj = new B();
         obj.show();
-        obj.sing();
+        obj.dance();
+       
     }
     
 }
